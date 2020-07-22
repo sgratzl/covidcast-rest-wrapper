1bf3a2642dc23498872c8674abaa0d317c13df61
Samuel Gratzl <sam@sgratzl.com>
2020-07-22 13:59:49 +0200

    play with params

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@
 covidcast rest wrapper
 """
 from enum import Enum
-from typing import List, Dict, Union, Optional
-from fastapi import FastAPI, HTTPException
+from typing import List, Dict, Union, Optional, Any
+from fastapi import FastAPI, HTTPException, Query
 from datetime import date, timedelta
 import aiohttp
 import asyncio
@@ -13,21 +13,36 @@ from model import MetaData, SignalData, SignalType, signal_to_data_source, TimeT
 app = FastAPI()
 session = aiohttp.ClientSession()
 
-url = "https://api.covidcast.cmu.edu/epidata/api.php"
+URL = "https://api.covidcast.cmu.edu/epidata/api.php"
 
 # covidcast.signal(data_source, signal, start_day=None, end_day=None, geo_type='county', geo_values='*')¶
 
-async def _fetch(params: Dict[str, Union[str, int, float]]):
-    params['cached'] = 'true'
-    print(str(params))
-    string_params = {k: v.value if isinstance(v, Enum) else str(v) for k, v in params.items()}
-    async with session.get(url, params=string_params) as resp:
+def _format_date(d: date) -> str:
+    return d.strftime('%Y%m%d')
+
+
+def _format_params(params: Dict[str, Any]) -> Dict[str, str]:
+    def _format_value(v: Any) -> str:
+        if isinstance(v, list):
+            return ','.join([_format_value(vi) for vi in v])
+        if isinstance(v, Enum):
+            return v.value
+        if isinstance(v, date):
+            return _format_date(v)
+        if isinstance(v, bool):
+            return 'true' if v else 'false'
+        return str(v)
+    return {k: _format_value(v) for k, v in params.items()}
+
+
+async def _fetch(params: Dict[str, Union[str, int, float, date, List[str], List[int], List[float], List[date]]]):
+    params['cached'] = True
+    async with session.get(URL, params=_format_params(params)) as resp:
         print(resp.url)
         res = await resp.json()
         result = res.get('result', 0)
         if result == -2:
-            # no results
-            return []
+            raise HTTPException(status_code=400, detail=f"no results: {resp.url}")
         if result != 1:
             raise HTTPException(status_code=500, detail=res["message"])
         return res.get("epidata", [])
@@ -51,17 +66,32 @@ async def metadata():
 
 # time_values: a,b,a-b
 
-def _get_day_offset(days = 1):
+def _get_day_offset(days=1) -> date:
     today = date.today()
-    day_of_before_yesterday = today - timedelta(days=days)
-    return day_of_before_yesterday.strftime('%Y%m%d')
+    return today - timedelta(days=days)
 
-@app.get('/signal/{signal}', response_model=List[SignalData])
-async def get_signal(signal: SignalType, time_type: TimeType, geo_type: GeoType, time_values: Optional[str] = None, geo_value: str = '*'):
+@app.get('/signal', response_model=List[SignalData])
+async def get_generic_signal(data_source: str, signal: str, time_type: TimeType, geo_type: GeoType, time_values: Optional[str] = None, geo_value: str = '*'):
     """
     return the raw meta data
     """
-    time_values = time_values or _get_day_offset(2)
+    return await _fetch({
+        'source': 'covidcast',
+        'data_source': data_source,
+        'signal': signal,
+        'time_type': time_type,
+        'geo_type': geo_type,
+        'time_values': time_values or _get_day_offset(2),
+        'geo_value': geo_value
+    })
+
+@app.get('/signal/{signal}', response_model=List[SignalData])
+async def get_signal(signal: SignalType, time_type: TimeType, geo_type: GeoType, time_values: Optional[List[date]] = Query(None), geo_value: str = '*'):
+    """
+    return the raw meta data
+    :param signal
+    :geo_value
+    """
     source = signal_to_data_source[signal]
     return await _fetch({
         'source': 'covidcast',
@@ -69,9 +99,10 @@ async def get_signal(signal: SignalType, time_type: TimeType, geo_type: GeoType,
         'signal': signal,
         'time_type': time_type,
         'geo_type': geo_type,
-        'time_values': time_values,
+        'time_values': time_values or [_get_day_offset(2)],
         'geo_value': geo_value
     })
+
 
 
 @app.get("/")
